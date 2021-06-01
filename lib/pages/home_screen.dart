89edb6f7f958812777c62aaf498a5e3c5d8eb76d
Vipul Jha <vipuljha08@gmail.com>
2021-06-01 17:28:45 +0530

    app: Nuke unused imports
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -5,8 +5,6 @@ import 'package:flutter_catalog/widgets/home_widgets/catalog_header.dart';
 import 'package:flutter_catalog/widgets/home_widgets/catalog_list.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_catalog/models/catalog.dart';
-import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 
 class HomeScreen extends StatefulWidget {
